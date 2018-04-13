a499fd4ee1c242419cbe218b2a5d2f417b0cae47
ccckmit <ccckmit@gmail.com>
2018-04-13 09:14:21 +0800

    fix example/object,json format

--- example/04-nodejs/06-json/e2c.js
+++ example/04-nodejs/06-json/e2c.js
@@ -1,14 +1,14 @@
-var e2c = { dog:"狗", cat:"貓", a: "一隻", chase:"追", eat:"吃" };
+var e2c = { dog: '狗', cat: '貓', a: '一隻', chase: '追', eat: '吃' }
 
-function mt(e) {
-  var c = [];
-  for (i in e) {
-    var eword = e[i];
-    var cword = e2c[eword];
-    c.push(cword);
+function mt (e) {
+  var c = []
+  for (let i in e) {
+    var eword = e[i]
+    var cword = e2c[eword]
+    c.push(cword)
   }
-  return c;
+  return c
 }
 
-var c = mt(process.argv.slice(2));
-console.log(c);
+var c = mt(process.argv.slice(2))
+console.log(c)
--- example/04-nodejs/06-json/json.js
+++ example/04-nodejs/06-json/json.js
@@ -1,14 +1,16 @@
 var george = {
-  "name": "George",
-  "age": 25,
-  "friends": [
-     {"name": "John", "age": 22 },
-     {"name": "Mary", "age": 28 }
+  'name': 'George',
+  'age': 25,
+  'friends': [
+     { 'name': 'John', 'age': 22 },
+     { 'name': 'Mary', 'age': 28 }
   ]
-};
+}
 
-console.log("george.age="+george.age);
-console.log("george.friends:");
-var friends = george.friends;
-for (i in friends)
-  console.log("    "+friends[i].name+" is "+friends[i].age+"years old!");
+console.log('george = %j', george)
+console.log('george.age=' + george.age)
+console.log('george.friends:')
+var friends = george.friends
+for (let i in friends) {
+  console.log('    ' + friends[i].name + ' is ' + friends[i].age + 'years old!')
+}
