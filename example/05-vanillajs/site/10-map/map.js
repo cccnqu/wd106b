8e7586b9e4abda045fe64f996b547511a8b8058d
ccckmit <ccckmit@gmail.com>
2018-05-25 11:27:02 +0800

    add 05-vanillajs / site /09-chart 10-map

--- example/05-vanillajs/site/10-map/map.js
+++ example/05-vanillajs/site/10-map/map.js
@@ -1,13 +1,13 @@
 const map = {}
 
 map.simple = function () {
-  var uluru = {lat: -25.363, lng: 131.044};
+  var pos = {lat: 24.45, lng: 118.32};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
+    zoom: 12,
+    center: pos
   });
   var marker = new google.maps.Marker({
-    position: uluru,
+    position: pos,
     map: map
   });
 }
